034f1cc464c5254ed20d94f610b3b8af20a343b1
goodbyebada <goodbyebada@naver.com>
2024-10-03 17:50:07 +0900

    모바일 기기 디버깅 태그 추가

--- src/Camera.js
+++ src/Camera.js
@@ -3,27 +3,29 @@ import Canvas from "./canvas1";
 export default function Camera() {
   const videoRef = useRef(null);
 
+  const selectRef = useRef(null);
+  let myStream;
+
   const [isCameraOn, setIsCameraOn] = useState(true);
   const [videoList, setVideoList] = useState([]);
-  const [camList, setCamList] = useState([]);
+  const [camList, setCamList] = useState([{ id: "fnf", label: "sdfs" }]);
+  const [camType, setCamType] = useState("");
 
   async function getCameras() {
     try {
       const devices = await navigator.mediaDevices.enumerateDevices();
 
       const cameras = devices.filter((device) => device.kind === "videoinput");
-      // const currentCamera = myStream.getVideoTracks()[0];
+      const currentCamera = myStream.getVideoTracks()[0];
       cameras.forEach((camera) => {
-        const info = { id: camera.deviceId, label: camera.label };
-        setCamList((prev) => [...prev, info]);
-        // const option = document.createElement("option");
-        // option.value = camera.deviceId;
-        // option.innerText = camera.label;
+        const option = document.createElement("option");
+        option.value = camera.deviceId;
+        option.innerText = camera.label;
 
-        // selectRef.current.appendChild(option);
-        // if (currentCamera.label === camera.label) {
-        //   option.selected = true;
-        // }
+        selectRef.current.appendChild(option);
+        if (currentCamera.label === camera.label) {
+          option.selected = true;
+        }
       });
     } catch (e) {
       console.log(e);
@@ -33,23 +35,31 @@ export default function Camera() {
   async function getMedia(deviceID) {
     const initialConstrains = {
       video: { facingMode: "environment" },
-    }; // 모바일 장치의 전면 카메라를 요청하기 위한 코드
+    };
 
     const cameraConstrains = {
       video: { deviceID: { exact: deviceID } },
     }; //해당 deviceid 카메라 요청 코드
 
+    if (!!deviceID) {
+      setCamType("TEST_TYPE");
+      return;
+    }
+
     try {
-      const stream = await navigator.mediaDevices.getUserMedia(
+      myStream = await navigator.mediaDevices.getUserMedia(
         deviceID ? cameraConstrains : initialConstrains
       ); //사용자에게 미디어 입력장치 사용권한을 요청
 
       if (videoRef.current) {
-        videoRef.current.srcObject = isCameraOn ? stream : null;
+        videoRef.current.srcObject = isCameraOn ? myStream : null;
       }
       if (!deviceID) {
         await getCameras(); //카메라들정보 가져옴
       }
+
+      const nowCamType = myStream.getVideoTracks()[0].label;
+      setCamType(nowCamType);
     } catch (err) {
       console.log(err); /* 오류 처리 */
     }
@@ -57,20 +67,6 @@ export default function Camera() {
 
   useEffect(() => {
     getMedia();
-    // const initCamera = async () => {
-    //   try {
-    //     const stream = await navigator.mediaDevices.getUserMedia({
-    //       video: { facingMode: "environment" },
-    //     });
-    //     if (videoRef.current) {
-    //       videoRef.current.srcObject = isCameraOn ? stream : null;
-    //     }
-    //   } catch (error) {
-    //     console.error("Error accessing camera:", error);
-    //   }
-    // };
-
-    // initCamera();
 
     return () => {
       // 컴포넌트가 언마운트되면 미디어 스트림 해제
@@ -94,16 +90,23 @@ export default function Camera() {
   };
 
   const ClickEvent = async (camerasSelect) => {
-    await getMedia(camerasSelect.id);
+    await getMedia(camerasSelect.value);
   };
 
   return (
     <div>
       <h1>FOR DEVICE TEST</h1>
-      <select>
-        {camList.map((elem) => (
-          <option onClick={() => ClickEvent(elem)}>{elem.label}</option>
-        ))}
+      <h2>{camType}</h2>
+      <select
+        ref={selectRef}
+        onChange={() => {
+          if (selectRef) {
+            const options = selectRef.current.options;
+            ClickEvent(options[options.selectedIndex]);
+          }
+        }}
+      >
+        <option>test</option>
       </select>
       <button onClick={toggleCamera}>
         {isCameraOn ? "Turn Off Camera" : "Turn On Camera"}
